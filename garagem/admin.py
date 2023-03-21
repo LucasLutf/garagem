7461e5996e6d6bcfab3c777dbf57daedf388e66e
LucasLutf <oolucaslutf@gmail.com>
2023-03-21 14:11:36 -0300

    Criação do modelo Marca

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from garagem.models import Marca, Descricao
+from garagem.models import Marca, Categoria
 
 admin.site.register(Marca)
-admin.site.register(Descricao)
+admin.site.register(Categoria)
