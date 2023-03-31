e32c359f38af24eb1c682f0b8293404c6915a041
LucasLutf <oolucaslutf@gmail.com>
2023-03-31 12:31:43 -0300

    6.8

--- garagem/models.py
+++ garagem/models.py
@@ -12,3 +12,27 @@ class Categoria(models.Model):
 
     def __str__(self):
         return self.descricao
+    
+class Acessorio(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+    
+class Cor(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+    
+class Veiculo(models.Model):
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    cor = models.ForeignKey(Cor, on_delete=models.CASCADE)
+    ano = models.IntegerField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.marca} {self.ano} {self.cor}"
+    
+    
